1c6e2967f7edba59e8a9e5b46bec41dd7e2b2c1b
Elif Gazioğlu <103602957+elifgazioglu@users.noreply.github.com>
2024-03-18 00:02:10 +0300

    add button icon and song

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import previousIcon from "./icons/previous.svg";
+import nextIcon from "./icons/next.svg";
+import playIcon from "./icons/play.svg";
+import Sound from "react-sound";
+import sound from "./songs/song2.mp3";
 
 function App() {
   const [gifs, setGifs] = useState([]);
-  const [currentGifIndex, setCurrentGifIndex] = useState(0);
+  const [currentGifIndex, setCurrentGifIndex] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,6 +20,7 @@ function App() {
         }
         const data = await response.json();
         setGifs(data.gifs);
+        selectRandomGif(data.gifs.length);
       } catch (error) {
         console.log(error);
       }
@@ -22,17 +29,43 @@ function App() {
     fetchData();
   }, []);
 
-  const selectRandomGif = () => {
-    const randomIndex = Math.floor(Math.random() * gifs.length);
+  const selectRandomGif = (length) => {
+    const randomIndex = Math.floor(Math.random() * (length || gifs.length));
     setCurrentGifIndex(randomIndex);
   };
 
   return (
     <div className="App">
       <div className="gif-container">
-        <img src={gifs[currentGifIndex]} />
+        {currentGifIndex !== null && (
+          <img className="gifs" src={gifs[currentGifIndex]} />
+        )}
       </div>
-      <button onClick={selectRandomGif}>Random gif</button>
+      <div className="button-container">
+        <button className="change-gif-btn previous-btn">
+          <img className="img-btn" src={previousIcon} alt="Previous" />
+        </button>
+        <button
+          className="change-gif-btn play-btn"
+          onClick={() => setIsPlaying(true)}
+        >
+          <img className="img-btn" src={playIcon} alt="Play" />
+        </button>
+        <button
+          className="change-gif-btn next-btn"
+          onClick={() => selectRandomGif()}
+        >
+          <img className="img-btn" src={nextIcon} alt="Next" />
+        </button>
+      </div>
+      <Sound
+        url={sound}
+        playStatus={isPlaying ? Sound.status.PLAYING : Sound.status.PAUSED}
+        playFromPosition={300 /* in milliseconds */}
+        // onLoading={this.handleSongLoading}
+        // onPlaying={this.handleSongPlaying}
+        // onFinishedPlaying={this.handleSongFinishedPlaying}
+      />
     </div>
   );
 }
